a8f52f924d813f4d33e71e40990cc8f5d0ee8763
cjc <curtis@Curtiss-MacBook-Pro.local>
2023-03-06 19:58:01 +0000

    refactor: can now be called from other project directories

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,28 @@
 import { Configuration, OpenAIApi } from "openai";
 import dotenv from "dotenv";
 import fs from "fs";
-dotenv.config();
+
+dotenv.config({ path: "~/Code/Personal/readme-bot/.env" });
 const OPENAI_KEY = process.env.OPENAI_KEY;
+
 const config = new Configuration({
     apiKey: OPENAI_KEY
 });
+const currentDir = process.cwd();
 
+const rootFiles = fs.readdirSync(`${currentDir}/`).join(" ");
+const srcFiles = fs.readdirSync(`${currentDir}/src`).join(" /src/");
 export const generateREADME = async () => {
     try {
-        const fileNames = fs.readdirSync("./src").join("");
-        console.log("generating README");
+        const allFiles = rootFiles.concat(srcFiles);
+
         const openai = new OpenAIApi(config);
         const response = await openai.createChatCompletion({
             model: "gpt-3.5-turbo",
             messages: [
                 {
                     role: "user",
-                    content: `Generate me a readme file in markdown based on the given list of file names ${fileNames}. You should be able to, create a title & description of what the project does, generate instructions for installation, running testing and have mention of any additional scripts in the package json. Should also mention any .env keys the user needs to add.`
+                    content: `Generate me a readme file in markdown based on the given list of file names ${allFiles}. You should be able to, create a title & description of what the project does, generate instructions for installation, running testing and have mention of any additional scripts in the package json. Should also mention any .env keys the user needs to add. Do not use the file names in the README just use them to make an assumption on what the project is. `
                 }
             ],
             temperature: 0,
@@ -42,7 +47,8 @@ const generatedREADME = await generateREADME();
 
 if (!generatedREADME) throw new Error("no README generated");
 
-fs.writeFile("README.md", generatedREADME, function (err) {
+console.log("currentDir: ", currentDir);
+fs.writeFile(`${currentDir}/README.md`, generatedREADME, function (err) {
     if (err) throw err;
     console.log("File is created successfully.");
 });
